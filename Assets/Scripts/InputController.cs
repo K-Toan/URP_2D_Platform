4581c6096a5ef45a2932d5c6e323cc56f618a293
K-Toan <988letoangt@gmail.com>
2024-05-13 11:22:39 +0700

    Added InputController

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    [Header("Keyboard Input Values")]
+    public Vector2 move;
+    public bool jump;
+    public bool dash;
+
+    [Header("Settings")]
+    public bool cursorLocked = true;
+
+    public void OnMove(InputValue value)
+    {
+        MoveInput(value.Get<Vector2>());
+    }
+
+    public void OnJump(InputValue value)
+    {
+        JumpInput(value.isPressed);
+    }
+
+    public void OnDash(InputValue value)
+    {
+        DashInput(value.isPressed);
+    }
+
+    public void MoveInput(Vector2 newMoveDirection)
+    {
+        move = newMoveDirection;
+    }
+
+    public void JumpInput(bool newJumpState)
+    {
+        jump = newJumpState;
+    }
+
+    public void DashInput(bool newSprintState)
+    {
+        dash = newSprintState;
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        SetCursorState(cursorLocked);
+    }
+
+    private void SetCursorState(bool newState)
+    {
+        Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,35 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
     public float MoveSpeed = 10f;
-    public float Acceleration = 15f;
+    public float ClimbSpeed = 3f;
+    public float Acceleration = 10f;
     [SerializeField] private Vector2 MoveDirection;
+    [SerializeField] private Vector2 LastMoveDirection;
 
     [Header("Jump & Gravity")]
-    public float JumpForce = 15f;
-    public float FallMultiplier = 5f;
-    public float LowJumpMultiplier = 7.5f;
+    public float JumpSpeed = 15f;
+    public float FallMultiplier = 2.5f;
+    public float LowJumpMultiplier = 2f;
+    public float JumpCooldownTime = 0.2f;
+    private bool enhanceGravity = true;
+
+    [Header("Dash")]
+    public float DashSpeed = 30f;
+    public float DashTime = 0.3f;
+    public float DashCooldownTime = 0.5f;
 
     [Header("Collision")]
     [SerializeField] private bool onGround;
     [SerializeField] private bool onWall;
     [SerializeField] private bool onRightWall;
     [SerializeField] private bool onLeftWall;
-    [SerializeField] private bool wallGrab;
-    [SerializeField] private int wallSide;
+    [SerializeField] private bool wallClimb;
     [SerializeField] private float collisionRadius = 0.25f;
     [SerializeField] private Vector2 bottomOffset;
     [SerializeField] private Vector2 rightOffset;
@@ -32,15 +41,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool canMove = true;
     [SerializeField] private bool canDash = true;
     [SerializeField] private bool canJump = true;
+    // [SerializeField] private bool wallJumped = false;
 
     [Header("Components")]
     private Rigidbody2D _rigidbody;
     private SpriteRenderer _spriteRenderer;
+    private InputController _input;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _input = GetComponent<InputController>();
 
         bottomOffset = new Vector2(0f, -0.5f);
         leftOffset = new Vector2(-0.5f, 0f);
@@ -52,7 +64,8 @@ public class PlayerController : MonoBehaviour
         HandleCollision();
         HandleWallSlide();
         HandleGravity();
-        Jump();
+        HandleJump();
+        Dash();
         Move();
 
         FlipSprite();
@@ -69,82 +82,116 @@ public class PlayerController : MonoBehaviour
 
         // if player is colliding with wall
         onWall = onRightWall || onLeftWall;
-
-        wallSide = onRightWall ? -1 : 1;
     }
 
     private void HandleWallSlide()
     {
-        wallGrab = Input.GetKey(KeyCode.W) && onWall;
+        // detect if player is on wall and climbing up
+        wallClimb = onWall && _input.move.y > 0;
 
         if (onWall && !onGround)
         {
-            if (wallGrab)
+            if (wallClimb)
             {
-                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, MoveSpeed);
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, ClimbSpeed);
             }
             else
             {
-                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, -3f);
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, -Mathf.Sqrt(ClimbSpeed));
             }
         }
     }
 
     private void HandleGravity()
     {
-        // Handle gravity and jump height
-        if (_rigidbody.velocity.y < 0)
+        if (enhanceGravity)
         {
-            _rigidbody.velocity += Vector2.up * Physics2D.gravity.y * FallMultiplier * Time.deltaTime;
-        }
-        else if (_rigidbody.velocity.y > 0 && !Input.GetButton("Jump"))
-        {
-            _rigidbody.velocity += Vector2.up * Physics2D.gravity.y * LowJumpMultiplier * Time.deltaTime;
-        }
-    }
-
-    private void Jump()
-    {
-        if (canJump)
-        {
-            if (Input.GetButtonDown("Jump"))
+            if (_rigidbody.velocity.y < 0)
             {
-                // Ground jump
-                if (onGround && !onWall)
-                {
-                    Debug.Log("Ground Jump");
-                    _rigidbody.velocity += Vector2.up * JumpForce;
-                }
-                // Wall jump
-                else if (onWall)
-                {
-                    Debug.Log("Wall Jump");
-
-                    Vector2 jumpDirection = new Vector2(onRightWall ? -1f : 1f, 1f).normalized;
-                    _rigidbody.velocity += jumpDirection * JumpForce;
-
-                    StartCoroutine(DisableMovement(0.25f));
-                }
+                _rigidbody.velocity += Vector2.up * Physics2D.gravity.y * FallMultiplier * Time.deltaTime;
+            }
+            else if (_rigidbody.velocity.y > 0 && !_input.jump)
+            {
+                _rigidbody.velocity += Vector2.up * Physics2D.gravity.y * LowJumpMultiplier * Time.deltaTime;
             }
         }
     }
 
+    private void HandleJump()
+    {
+        if (canJump && _input.jump)
+        {
+            if (onGround)
+            {
+                GroundJump();
+            }
+            else if (onWall)
+            {
+                WallJump();
+            }
+            StartCoroutine(JumpCooldown(JumpCooldownTime));
+        }
+    }
+
+    private void GroundJump()
+    {
+        Debug.Log("Ground Jump");
+
+        _rigidbody.velocity += Vector2.up * JumpSpeed;
+
+        StartCoroutine(JumpCooldown(0.5f));
+    }
+
+    private void WallJump()
+    {
+        Debug.Log("Wall Jump");
+
+        _rigidbody.velocity = new Vector2(onRightWall ? -1f : 1f, 1f).normalized * JumpSpeed;
+
+        StartCoroutine(DisableMovement(0.25f));
+    }
+
     private void Dash()
     {
+        if (canDash && _input.dash)
+        {
+            // Vector2 dashDirection = new Vector2(MoveDirection.x, MoveDirection.y).normalized;
+            Vector2 dashDirection = LastMoveDirection.normalized;
 
+            // start dashing
+            StartCoroutine(Dash(dashDirection));
+        }
     }
 
     private void Move()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        if (_input.move != Vector2.zero)
+            LastMoveDirection = _input.move;
 
-        MoveDirection = new Vector2(x, y);
+        MoveDirection = _input.move;
 
-        if (canMove)
+        if (!canMove || wallClimb)
+            return;
+
+        if (onGround)
         {
-            // _rigidbody.velocity = new Vector2(MoveDirection.x * MoveSpeed, _rigidbody.velocity.y);
+            _rigidbody.velocity = new Vector2(MoveDirection.x * MoveSpeed, _rigidbody.velocity.y);
+        }
+        else
+        {
             _rigidbody.velocity = Vector2.Lerp(_rigidbody.velocity, (new Vector2(MoveDirection.x * MoveSpeed, _rigidbody.velocity.y)), Acceleration * Time.deltaTime);
+        }
+    }
+
+    private void FlipSprite()
+    {
+        if (MoveDirection.x > 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else if (MoveDirection.x < 0)
+        {
+            _spriteRenderer.flipX = false;
         }
     }
 
@@ -162,6 +209,24 @@ public class PlayerController : MonoBehaviour
         canJump = true;
     }
 
+    public IEnumerator Dash(Vector2 dashDirection)
+    {
+        StartCoroutine(DashCooldown(DashCooldownTime));
+
+        // temp
+        float gravityScale = _rigidbody.gravityScale;
+
+        // start dashing
+        _rigidbody.gravityScale = 0f;
+        _rigidbody.velocity = dashDirection * DashSpeed;
+
+        yield return new WaitForSeconds(DashTime);
+
+        // reset
+        _rigidbody.gravityScale = gravityScale;
+        _rigidbody.velocity = Vector2.zero;
+    }
+
     public IEnumerator DashCooldown(float time)
     {
         canDash = false;
@@ -169,19 +234,7 @@ public class PlayerController : MonoBehaviour
         canDash = true;
     }
 
-    void FlipSprite()
-    {
-        if (MoveDirection.x > 0)
-        {
-            _spriteRenderer.flipX = true;
-        }
-        else if (MoveDirection.x < 0)
-        {
-            _spriteRenderer.flipX = false;
-        }
-    }
-
-    void OnDrawGizmos()
+    public void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
 
